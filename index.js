ae8f872826222f835a1b8191f7ac3db3d3cec698
Galina Demidova <104835117+Novemberawakee@users.noreply.github.com>
2022-07-10 19:24:21 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let text = "I am a web developer...";
+let i = 0;
+let speed = 100;
+
+function type() {
+    if(i < text.length) {
+        document.querySelector("#par").textContent += text.charAt(i);
+        i++;
+        setTimeout(type,speed);
+    }
+}
+type();
